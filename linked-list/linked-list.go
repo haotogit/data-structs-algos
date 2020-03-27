a42a026dbf8d62db23c348d37f4c9f9a39d63ff8
Samuel Park <samuel.d.g.park@gmail.com>
2020-03-27 04:30:55 -0700

    fix Size(), Add()+tests, GetNth()+test

--- linked-list/linked-list.go
+++ linked-list/linked-list.go
@@ -1,10 +1,12 @@
-package main
+package linkedList
 
-type LinkedListMethods interface {
+import "fmt"
+
+type MyLinkedList interface {
     Size() int
-    Get(i int) *Node
-    Add()
-    AddAtI(i int, el Node)
+    GetNth(i int) *Node
+    Add(el interface{}) bool
+    AddAtI(i int, el interface{})
     //Set(i int, el)
     //Remove(i int, el)
     //Clear()
@@ -17,12 +19,6 @@ type LinkedListMethods interface {
     // TODO get(i)
     // i - index for desired element
     // return element
-
-    // TODO add(el)
-    // append el to list
-
-    // TODO add(i, el)
-    // append el to ith position in list
 
     // TODO set(i, el)
     // set el to ith position in list
@@ -39,49 +35,72 @@ type LinkedListMethods interface {
 type Node struct {
     prev *Node
     next *Node
-    data int
+    data interface{}
 }
 
 type LinkedList struct {
-    list []Node
-}
-
-func main() {
-    // a linked list is a sequence of nodes
-    // that contain references to one another
-    // double || single, with references to prev && next || next
-
-    // unlike an array, linked lists do not provide constant time access to
-    // a specific index because you have to traverse through K items
-
-    // benefit of linkedlist is add and remove from beginning of list is constant
-
-    // type Node
-    // type linkedList
+    head *Node
+    tail *Node
+    size int
 }
 
 func (l *LinkedList) Size() int {
-    return len(l.list)
+    return l.size 
 }
 
-func (l *LinkedList) Get(i int) Node {
-    return l.list[i]
-}
-
-func (l *LinkedList) Add(el Node) {
-    // handle null value
-    // handle increasing capacity
-    l.list = append(l.list, el)
-}
-
-func (l *LinkedList) AddAtI(i int, el Node) {
-    // handle null value
-    // handle increasing capacity
-    // handle index out of range
-    l.Add(el)
-    if i < len(l.list) {
-        copy(l.list[i+1:], l.list[i:])
-        l.list[i] = el
+func isNil(el interface{}) interface{} {
+    var nilVal interface{}
+    switch t := el.(type) {
+        case string:
+            nilVal = ""
+            // had to include this because of t...
+            fmt.Println(t)
+        case int:
+            nilVal = 0
+        default:
+            nilVal = nil
     }
-    //l.Add(el)
+
+    return nilVal
 }
+
+func (l *LinkedList) GetNth(i int) *Node {
+    target := l.head;
+    for target != nil && i >= 0 && target.next != nil {
+        target = target.next
+        i--
+    }
+
+    return target
+}
+
+func (l *LinkedList) Add(el interface{}) bool {
+    if el == isNil(el) {
+        return false
+    }
+
+    if l.head == nil {
+        l.head = &Node{nil, nil, el}
+    } else if l.tail == nil {
+        // why not &l.head
+        l.tail = &Node{l.head, nil, el}
+    } else {
+        target := &Node{l.tail, nil, el}
+        l.tail.next = target
+    }
+
+    l.size++
+    return true
+}
+
+//func (l *LinkedList) AddAtI(i int, el Node) {
+//    // handle null value
+//    // handle increasing capacity
+//    // handle index out of range
+//    l.Add(el)
+//    if i < len(l.list) {
+//        copy(l.list[i+1:], l.list[i:])
+//        l.list[i] = el
+//    }
+//    //l.Add(el)
+//}
--- linked-list/linked-list_test.go
+++ linked-list/linked-list_test.go
@@ -1,4 +1,4 @@
-package main
+package linkedList
 
 import (
     "testing"
@@ -6,55 +6,80 @@ import (
     "time"
 )
 
-func makeLinkedList(withCapacity int) (LinkedList, int) {
-    var newList LinkedList
-    var i, capacity int
-    rand.Seed(time.Now().UnixNano())
+func makeLinkedList(withCapacity int, fill bool) (*LinkedList, int) {
+    var capacity int
+    i := 0
     if withCapacity > 0 {
         capacity = withCapacity
     } else {
-        capacity = rand.Intn(50)
+        capacity = getRandIntn(0)
     }
 
-    newList = LinkedList{ make([]Node, 0, capacity) }
-    i = 0
-    for i < capacity {
-        // what happens if past capacity ??
-        newList.list = append(newList.list, Node{nil, nil, i})
-        i++
+    newList := &LinkedList{ nil, nil, 0 }
+    if fill {
+        for i < capacity {
+            // what happens if past capacity ??
+            newList.Add(Node{nil, nil, i})
+            i++
+        }
     }
 
     return newList, i
 }
 
+func getRandIntn(max int) int {
+    rand.Seed(time.Now().UnixNano())
+    if max == 0 {
+        return rand.Intn(50)
+    } else {
+        return rand.Intn(max)
+    }
+}
+
 func TestSize(t *testing.T) {
-    var newList LinkedList
-    var actualSize int
     for iterations := 0; iterations < 100; iterations++ {
-        newList, actualSize = makeLinkedList(0)
+        newList, actualSize := makeLinkedList(0, true)
         if newList.Size() != actualSize {
             t.Errorf("Expected: %d Actual: %d\n", actualSize, newList.Size())
         }
     }
 }
 
-func TestGet(t *testing.T) {
-    var newList LinkedList
-    var actualSize, randIdx int
-    var currItem Node
+func TestAdd(t *testing.T) {
     for iterations := 0; iterations < 100; iterations++ {
-        newList, actualSize = makeLinkedList(0)
-        if actualSize == 0 {
-            continue
-        } else if actualSize > 1 {
+        newList, _ := makeLinkedList(0, false)
+        for i := 1; i < 10; i++ {
+            if !newList.Add(i) {
+                t.Errorf("Failed to add %d\n", i)
+            }
+        }
+    }
+}
+
+func TestAddNil(t *testing.T) {
+    newList, _ := makeLinkedList(0, false)
+    if newList.Add(0) || newList.Add("") {
+        t.Errorf("No nil value allowed\n")
+    }
+}
+
+func TestGetNth(t *testing.T) {
+    for iterations := 0; iterations < 100; iterations++ {
+        newList, actualSize := makeLinkedList(0, true)
+        if actualSize > 1 {
             actualSize--
         }
 
-        rand.Seed(time.Now().UnixNano())
-        randIdx = rand.Intn(actualSize)
-        currItem = newList.Get(randIdx)
-        if currItem != newList.list[randIdx] {
-            t.Errorf("Expected %p but got %p\n", &newList.list[randIdx], &currItem)
+        randIdx := getRandIntn(actualSize)
+        currItem := newList.GetNth(randIdx)
+        target := newList.head
+        for target != nil && randIdx >= 0 && target.next != nil {
+            target = target.next
+            randIdx--
+        }
+
+        if currItem != target {
+            t.Errorf("Expected %+v, but got %+v\n", target, currItem)
         }
     }
 }
